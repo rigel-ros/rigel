0238d145bccae448ffe7fe45c71da59d7fc28e0c
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2022-01-25 12:13:13 +0000

    Fixed import of .rosinstall files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('./README.md', 'r') as readme:
 
 setuptools.setup(
     name='sirius',
-    version='0.0.4',
+    version='0.0.5',
     author='Pedro Melo',
     author_email='pedro.m.melo@inesctec.pt',
     description='Containerize and deploy your ROS application using Docker.',
