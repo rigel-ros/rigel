7c008957414118f845a193919fcb06c7a04d91cf
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2022-01-28 13:22:29 +0000

    Deploy mechanism is working.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from base64 import b64decode
 from boto3 import client as aws_client
 from botocore.exceptions import ClientError
 from docker import APIClient, from_env
@@ -47,7 +48,8 @@ def main() -> None:
 
         # Extracted and adapted from:
         # https://stackoverflow.com/questions/43540254/how-to-stream-the-logs-in-docker-python-api
-        docker_client = APIClient(base_url='tcp://docker:2375')
+        # docker_client = APIClient(base_url='tcp://docker:2375')
+        docker_client = APIClient(base_url='unix:///var/run/docker.sock')
         image = docker_client.build(
             path='.',
             dockerfile='.sirius_config/Dockerfile',
@@ -78,31 +80,51 @@ def main() -> None:
         complete_image_name = f'{registry_name}/{image_name}:{image_tag}'
 
         try:
-            print(f'Obtaining authorization from AWS ECR')
+
+            # Extracted and adapted from:
+            # https://github.com/AlexIoannides/py-docker-aws-example-project/blob/master/deploy_to_aws.py
+            print(f'Authenticate with a registry.AWS ECR ({configuration_parser.configuration_file.registry.name})')
+
             aws_ecr = aws_client(
                 'ecr',
                 aws_access_key_id=environ.get('AWS_ACCESS_KEY'),
                 aws_secret_access_key=environ.get('AWS_SECRET_KEY'),
                 region_name=environ.get('AWS_REGION')
             )
-            authorization_token = aws_ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']
+            aws_token = b64decode(aws_ecr.get_authorization_token()['authorizationData'][0]['authorizationToken']).replace(b'AWS:', b'').decode('utf-8')
 
-            print(f'Pushing image {complete_image_name} to ECR.')
             docker_client = from_env()
-            image = docker_client.images.get(complete_image_name)
-            image.push(
-                registry_name,
-                auth_config={
-                    'username': configuration_parser.configuration_file.registry.user,
-                    'password': authorization_token
-                }
+            docker_client.login(
+                username=configuration_parser.configuration_file.registry.user,
+                password=aws_token,
+                registry=configuration_parser.configuration_file.registry.name
             )
-
-            print(f'Image {complete_image_name} was pushed with success to ECR.')
 
         except ClientError:
             print('Invalid AWS credentials.')
             exit(1)
+
+        print(f'Pushing image {complete_image_name} to ECR.')
+        image = docker_client.images.push(
+            complete_image_name,
+            stream=True,
+            decode=True,
+            auth_config={
+                'username': configuration_parser.configuration_file.registry.user,
+                'password': aws_token
+            }
+        )
+        image_logs = iter(image)
+        while True:
+            try:
+                log = next(image_logs)
+                if 'progess' in log:
+                    print(log['progress'].strip('\n'))
+            except StopIteration:
+                print(f'Image {complete_image_name} was pushed with success to ECR.')
+                break
+            except ValueError:
+                print(f'Unable to parse log from Docker image being build ({complete_image_name}): {log}')
 
     else:
         print(f'Invalid command "{cli_args.command}"')
