ee46b11e73fbde6363fcdd8b84720e302f862bd9
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2022-02-01 10:12:52 +0000

    AWS ECR plugin was moved to separate repository.

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
     ],
     python_requires='>=3.8',
     install_requires=[
-        'boto3==1.20.44',
         'docker==4.1.0',
         'jinja2==3.0.3',
         'pyyaml==6.0'
