250ada3c4dd2273f184351d831458919a1d00552
Pedro Pereira <pedro.m.melo@inesctec.pt>
2022-02-01 22:19:36 +0000

    Updated version 0.0.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('./README.md', 'r') as readme:
 
 setuptools.setup(
     name='rigel',
-    version='0.0.11',
+    version='0.0.12',
     author='Pedro Melo',
     author_email='pedro.m.melo@inesctec.pt',
     description='Containerize and deploy your ROS application using Docker.',
