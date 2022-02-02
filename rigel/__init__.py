a42cea572589262f5694c3f59a8a9ae763851a9d
Pedro Pereira <pedro.m.melo@inesctec.pt>
2022-02-02 09:56:42 +0000

    Removed import of deleted submodule.

--- rigel/__init__.py
+++ rigel/__init__.py
@@ -1,6 +1,5 @@
 from . import files  # noqa: 401
 from . import parsers  # noqa: 401
-from . import renderers  # noqa: 401
 from .exceptions import (  # noqa: 401
     EmptyRigelfileError,
     IncompleteRigelfileError,
