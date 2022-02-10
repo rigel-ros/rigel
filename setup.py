d693a0d385416ee1529418b3e10856c039580cc0
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2022-02-10 09:26:49 +0000

    Removed package versions from setup.py.

--- setup.py
+++ setup.py
@@ -24,11 +24,11 @@ setuptools.setup(
     ],
     python_requires='>=3.8',
     install_requires=[
-        'Click==7.0',
-        'docker==4.1.0',
-        'jinja2==3.0.3',
-        'pyyaml==6.0',
-        'rich==11.0.0'
+        'Click',
+        'docker',
+        'jinja2',
+        'pyyaml',
+        'rich'
     ],
     py_modules=['main'],
     entry_points={
